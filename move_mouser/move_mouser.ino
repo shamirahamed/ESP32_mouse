1899547d681be1d29814624a3eaabb1ed44d79ef
Shamir <x21154929@student.ncirl.ie>
2023-12-22 16:12:12 +0000

    first commit

---
+++ move_mouser/move_mouser.ino
@@ -0,0 +1,17 @@
+#include <BleMouse.h>
+
+BleMouse bleMouse("MS", "MS", 85);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+  bleMouse.begin();
+}
+
+void loop() {
+  if(bleMouse.isConnected()) {
+    Serial.println("Scroll Down");
+    bleMouse.move(0,0,-1);
+  }
+  delay(2000);
+}
